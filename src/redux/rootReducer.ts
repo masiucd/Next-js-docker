a7eafe561a50d88514120ca7f573d2a49499e1ca
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-11-23 09:11:10 +0100

    Clear

--- src/redux/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-import { combineReducers } from 'redux';
-import expenseReducer from './expenseCalculator/expense.reducer';
-
-export default combineReducers({
-  expenseTracker: expenseReducer,
-});
