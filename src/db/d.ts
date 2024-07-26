0e9b5ff19fe75de088c0d08a77b27f1116a5616b
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-07-26 14:01:33 +0200

    chore: Add database configuration and connection setup

---
+++ src/db/d.ts
@@ -0,0 +1,17 @@
+import {drizzle} from "drizzle-orm/postgres-js";
+import {migrate} from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+
+// for migrations
+// const migrationClient = postgres(
+//   "postgres://postgres:adminadmin@0.0.0.0:5432/db",
+//   {max: 1}
+// );
+
+// migrate(drizzle(migrationClient));
+
+// TODO use env vars
+// for query purposes
+const queryClient = postgres("postgres://admin:admin@0.0.0.0:5432/mydb");
+const db = drizzle(queryClient);
+// await db.select().from()
