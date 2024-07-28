e4fa11df37c6b8ba5f3ad4471dcdc129efb7c3f3
masiucd <ciszekmarcell@gmail.com>
2024-07-28 09:34:54 +0200

    refactor: Update getExpires function to use more descriptive parameter name and add JSDoc comments

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,6 +1,12 @@
 import "server-only";
 
-export function getExpires(seconds: number = 600): Date {
+/**
+ * Calculates the expiration date based on the current time and the given number of seconds.
+ *
+ * @param {number} [seconds=3600] - The number of seconds from now until the expiration date. Defaults to 3600 seconds (1 hour).
+ * @returns {Date} The calculated expiration date.
+ */
+export function getExpires(seconds: number = 3600): Date {
   let milliseconds = 1000;
-  return new Date(Date.now() + seconds * milliseconds); // 10 minutes
+  return new Date(Date.now() + seconds * milliseconds); // 1 hour
 }
