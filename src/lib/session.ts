93c82d8263952b5be059b4cc327c7a71a0080092
masiucd <ciszekmarcell@gmail.com>
2024-07-28 13:17:56 +0200

    setAuthSession function

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,5 +1,10 @@
 import "server-only";
 
+import {format} from "date-fns";
+import {cookies} from "next/headers";
+
+import {encrypt} from "./jwt";
+
 /**
  * Calculates the expiration date based on the current time and the given number of seconds.
  *
@@ -9,4 +14,22 @@ import "server-only";
 export function getExpires(seconds: number = 3600): Date {
   let milliseconds = 1000;
   return new Date(Date.now() + seconds * milliseconds); // 1 hour
+}
+
+export async function setAuthSession(userFields: {
+  userId: number;
+  password: string;
+  email: string;
+}) {
+  let expires = getExpires();
+  let cookieStore = cookies();
+  cookieStore.set({
+    name: "session",
+    value: await encrypt({
+      userId: userFields.userId,
+      email: userFields.email,
+      expires: format(expires, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+    }),
+    expires,
+  });
 }
