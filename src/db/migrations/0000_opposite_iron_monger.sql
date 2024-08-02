70639d8b0253e1ed74ea3310ae3d3f48805826d6
masiucd <ciszekmarcell@gmail.com>
2024-08-02 15:50:05 +0200

    refactor: Rename migration file to reflect updated table name and constraints

--- src/db/migrations/0000_opposite_iron_monger.sql
+++ src/db/migrations/0000_opposite_iron_monger.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS "tasks" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"task" varchar(255) NOT NULL,
-	"completed" boolean DEFAULT false,
+	"completed" boolean DEFAULT false NOT NULL,
 	"user_id" integer NOT NULL
 );
-
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
@@ -14,21 +13,13 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"created_at" timestamp DEFAULT now(),
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
-
 --> statement-breakpoint
-DO $ $ BEGIN
-ALTER TABLE
-	"tasks"
-ADD
-	CONSTRAINT "tasks_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
-
+DO $$ BEGIN
+ ALTER TABLE "tasks" ADD CONSTRAINT "tasks_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
-WHEN duplicate_object THEN null;
-
-END $ $;
-
+ WHEN duplicate_object THEN null;
+END $$;
 --> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "name_idx" ON "users" USING btree ("name");
-
---> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "task_idx" ON "tasks" USING btree ("task");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "user_name_idx" ON "users" USING btree ("name");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "email_idx" ON "users" USING btree ("email");
