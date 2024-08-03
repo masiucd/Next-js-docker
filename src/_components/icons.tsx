e565cf807c82194fc1417f258e057e4a5443dbbc
masiucd <ciszekmarcell@gmail.com>
2024-08-03 15:40:07 +0200

    new icons

--- src/_components/icons.tsx
+++ src/_components/icons.tsx
@@ -1,5 +1,6 @@
 import {
   Bomb,
+  Check,
   Clock,
   Copy,
   Delete,
@@ -11,12 +12,13 @@ import {
 } from "lucide-react";
 
 export let Icons = Object.freeze({
+  Add: (props: LucideProps) => <Plus size={ICON_SIZE} {...props} />,
+  Complete: (props: LucideProps) => <Check size={ICON_SIZE} {...props} />,
   Copy: (props: LucideProps) => <Copy size={ICON_SIZE} {...props} />,
   Delete: (props: LucideProps) => <Delete size={ICON_SIZE} {...props} />,
   Edit: (props: LucideProps) => <Edit size={ICON_SIZE} {...props} />,
   Info: (props: LucideProps) => <Info size={ICON_SIZE} {...props} />,
   Snooze: (props: LucideProps) => <Clock size={ICON_SIZE} {...props} />,
-  Add: (props: LucideProps) => <Plus size={ICON_SIZE} {...props} />,
   Warning: (props: LucideProps) => (
     <TriangleAlert size={ICON_SIZE} {...props} />
   ),
