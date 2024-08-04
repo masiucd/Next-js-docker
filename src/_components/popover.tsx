1a34ad5f5bde63d3b566733886f82447be9edb78
masiucd <ciszekmarcell@gmail.com>
2024-08-04 10:15:30 +0200

    refactor: Update PopOver component to use PropsWithChildren

--- src/_components/popover.tsx
+++ src/_components/popover.tsx
@@ -1,31 +1,24 @@
 import {Button, Flex, Popover as RadixPopOver} from "@radix-ui/themes";
-import type {ReactNode} from "react";
+import type {PropsWithChildren, ReactNode} from "react";
 
-import {P} from "./typography";
-
-export function PopOver({
-  triggerButton,
-  actionButton,
-}: {
+type Props = {
   triggerButton: ReactNode;
   actionButton: ReactNode;
-}) {
+};
+export function PopOver(props: PropsWithChildren<Props>) {
   return (
     <RadixPopOver.Root>
-      <RadixPopOver.Trigger>{triggerButton}</RadixPopOver.Trigger>
+      <RadixPopOver.Trigger>{props.triggerButton}</RadixPopOver.Trigger>
       <RadixPopOver.Content width="360px">
-        <Flex gap="3" direction="column">
-          <P>
-            Are you sure you want to delete this task? This action cannot be
-            undone.
-          </P>
-          <Flex gap="3" mt="3" justify="between" className="ml-auto ">
-            <RadixPopOver.Close>{actionButton}</RadixPopOver.Close>
+        <Flex gap="3" direction="column" className="bg-red-200">
+          {props.children}
+          <Flex gap="2" ml="auto" width="200px" justify="end">
             <RadixPopOver.Close>
               <Button size="1" variant="outline">
                 Cancel
               </Button>
             </RadixPopOver.Close>
+            <RadixPopOver.Close>{props.actionButton}</RadixPopOver.Close>
           </Flex>
         </Flex>
       </RadixPopOver.Content>
