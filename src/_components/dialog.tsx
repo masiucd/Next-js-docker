d6733ee6e137655c50d8c86f3a0cdb2445bae559
masiucd <ciszekmarcell@gmail.com>
2024-08-04 10:15:20 +0200

    refactor: Update Dialog component to include Dialog.Close subcomponent

--- src/_components/dialog.tsx
+++ src/_components/dialog.tsx
@@ -5,20 +5,26 @@ type Props = {
   buttonComponent: ReactNode;
   title: string;
   description: string;
+  open?: boolean;
 };
 
 export function Dialog(props: PropsWithChildren<Props>) {
   return (
-    <RadixDialog.Root>
+    <RadixDialog.Root open={props.open}>
       <RadixDialog.Trigger>{props.buttonComponent}</RadixDialog.Trigger>
       <RadixDialog.Content maxWidth="450px">
         <RadixDialog.Title>{props.title}</RadixDialog.Title>
         <RadixDialog.Description size="2" mb="4">
           {props.description}
         </RadixDialog.Description>
-
         {props.children}
       </RadixDialog.Content>
     </RadixDialog.Root>
   );
 }
+
+function DialogClose(props: PropsWithChildren) {
+  return <RadixDialog.Close>{props.children}</RadixDialog.Close>;
+}
+
+Dialog.Close = DialogClose;
